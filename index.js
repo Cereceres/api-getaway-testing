cffc482c5c133e97b669a620d3731799e2ff216b
cereceres <jesus.edelcereceres@gmail.com>
2018-03-18 21:50:19 -0700

    errror is fixed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const addHandler = require('./lib/add-handler');
-const morgan = require('morgan')
- module.exports = (pathToLambdaH adlerMap = {}, port = 3000, app = express()) => {
-    app.use(morgan('tiny'))
+const morgan = require('morgan');
+module.exports = (pathToLambdaHadlerMap = {}, port = 3000, app = express()) => {
+    app.use(morgan('tiny'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     addHandler(app, pathToLambdaHadlerMap);
