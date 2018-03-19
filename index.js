cd8959909d67a722d65886a6d75ee2a79e377fbe
cereceres <jesus.edelcereceres@gmail.com>
2018-03-18 21:19:07 -0700

    extended is setted

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const addHandler = require('./lib/add-handler');
 
  module.exports = (pathToLambdaHadlerMap = {}, port = 3000, app = express()) => {
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded());
+    app.use(bodyParser.urlencoded({ extended: true }));
     addHandler(app, pathToLambdaHadlerMap);
     app.listen(port, () => console.log('Server started on port ', port));
 
